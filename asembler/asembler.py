6167f1569daf85d51696d78164fdfa3a8447c651
Navaneeth Narayan K <navaneethnarayank@gmail.com>
2024-02-10 22:35:28 +0530

    completed assembler code which is a text editor where you can write, save, open, and compile the code. the compiled code is saved as bincode.txt (binary code)

--- asembler/asembler.py
+++ asembler/asembler.py
@@ -50,14 +50,62 @@ class TextEditor:
 
     def compile_file(self):
         c = assembler(self.text_area.get(1.0, tk.END))
-        c.removeWhiteSpace()
+        bin_code_string = c.assemble()
+        f = open("bincode.txt",'w')
+        f.write(bin_code_string)
+        f.close()
         print("program compiled")
+    
 
+dest_opcode = {"0":"000",
+                  "M":"001",
+                  "D":"010",
+                  "MD":"011",
+                  "DM":"011",
+                  "A":"100",
+                  "AM":"101",
+                  "MA":"101",
+                  "AD":"110",
+                  "DA":"110",
+                  "ADM":"111",
+                  "AMD":"111",
+                  "DAM":"111",
+                  "DMA":"111",
+                  "MAD":"111",
+                  "MDA":"111"}
+    
+jmp_opcode = {"NULL":"000",
+                   "JGT":"001",
+                   "JEQ":"010",
+                   "JGE":"011",
+                   "JLT":"100",
+                   "JNE":"101",
+                   "JLE":"110",
+                   "JMP":"111"}
+    
+alu_opcode = {"0":"101010",
+                  "1":"111111",
+                  "-1":"111010",
+                  "D":"001100",
+                  "Y":"110000",
+                  "!D":"001101",
+                  "!Y":"110001",
+                  "-D":"001111",
+                  "-Y":"110011",
+                  "D+1":"011111",
+                  "Y+1":"110111",
+                  "D-1":"001110",
+                  "Y-1":"110010",
+                  "D+Y":"000010",
+                  "D-Y":"010011",
+                  "Y-D":"000111",
+                  "D&Y":"000000",
+                  "D|Y":"010101"}
 class assembler:
     def __init__(self,str):
         self.str = str
     
-    def removeWhiteSpace(self):
+    def assemble(self):
         #remove comments
         temp_str = ''
         found_next_line = 1
@@ -84,6 +132,13 @@ class assembler:
             temp_str = temp_str+i
         self.str = temp_str
 
+        #remove spaces
+        temp_str = ''
+        for i in self.str:
+            if i != ' ':
+                temp_str = temp_str + i
+        self.str = temp_str
+        
         #make the code string to a list for string line by line
         code_list = self.str.split('\n')
         code_list = [x for x in code_list if x != '']
@@ -104,10 +159,57 @@ class assembler:
                 if code_list[i][1:] in jmp_dic:
                     code_list[i] = '@'+str(jmp_dic[code_list[i][1:]])
 
-        print(code_list)
-        print(jmp_dic)
+        #Ideal asembly code conversion
+        for i in range(len(code_list)):
+            if code_list[i][0] != '@':
+                #C instruction
+                if '=' not in code_list[i]:
+                    code_list[i] = "0="+code_list[i]
+                if ';' not in code_list[i]:
+                    code_list[i]=code_list[i]+';NULL'
+        bin_code_str = compile(code_list)
+        return bin_code_str
+        
 
-    def compile(self):
-        pass
+    
+    
+def hex2bin(strng):
+    addr_int  = int('0x'+strng,16)
+    addr_bin = bin(addr_int)
+    addr_bin = addr_bin[2:]
+    addr_bin = (15-len(addr_bin))*'0'+addr_bin
+    return addr_bin        
+
+def compile(code_list):
+    global jmp_opcode, dest_opcode, alu_opcode
+    bin_code = []
+    for i in range(len(code_list)):
+        if code_list[i][0]=='@':
+            bin_code.append('0'+hex2bin(code_list[i][1:]))
+        else:
+            temp_code = code_list[i].split('=')
+            dst_code = temp_code[0]
+            temp_code = temp_code[1].split(';')
+            alu_code = temp_code[0]
+            jmp_code = temp_code[1]
+            temp_alu = ''
+            a = '0'
+            for j in alu_code:
+                if j == 'A': 
+                    a = '0'
+                    temp_alu = temp_alu + 'Y'
+                    continue
+                elif j == 'M':
+                    a = '1'
+                    temp_alu = temp_alu + 'Y'
+                    continue
+                temp_alu = temp_alu + j
+            alu_code = temp_alu
+            bin_code.append('111'+a+alu_opcode[alu_code]+dest_opcode[dst_code]+jmp_opcode[jmp_code])
+    bin_code_string = ''
+    for i in bin_code:
+        bin_code_string = bin_code_string + i + '\n'
+    return bin_code_string          
+
 if __name__ == "__main__":
     text_editor = TextEditor()
